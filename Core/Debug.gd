4d4fbd8eaf8da9178f56c618639a725ef026023d
TheDuriel <manueldurielfischer@gmail.com>
2020-02-24 22:17:18 +0100

    Add Debug Autoload

---
+++ Core/Debug.gd
@@ -0,0 +1,13 @@
+extends Node
+"""
+	Debug Autoload Script
+"""
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_F1:
+			
+			if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			else:
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
