c9bd1ac68e5d8aca6a53f604f2069d1c0104b617
TheDuriel <manueldurielfischer@gmail.com>
2020-02-25 15:38:23 +0100

    Add New FPSPlayer

--- Core/FPSModelLayer/FPSModelLayer.gd
+++ Core/FPSModelLayer/FPSModelLayer.gd
@@ -9,12 +9,14 @@ var fps_viewport: Viewport setget set_fps_viewport
 onready var _texturerect: TextureRect = $TextureRect
 
 
+	# Called by FPSCamera
 func set_fps_viewport(new_value: Viewport) -> void:
 	if is_instance_valid(new_value):
 		fps_viewport = new_value
 		_texturerect.texture = fps_viewport.get_texture()
 
 
+	# Resize the Viewport to the Size of the Window.
 func _on_TextureRect_resized() -> void:
 	if is_instance_valid(fps_viewport):
 		fps_viewport.size = _texturerect.rect_size
