1b7b4dd2689e5b278c1e99494877a6cfbffe7757
TheDuriel <manueldurielfischer@gmail.com>
2020-03-04 17:49:53 +0100

    Fix material depth sorting

--- SharedAssets/Materials/VRFadeMaterial.gd
+++ SharedAssets/Materials/VRFadeMaterial.gd
@@ -5,6 +5,8 @@ class_name VRMaterial
 func _init() -> void:
 	params_diffuse_mode = SpatialMaterial.DIFFUSE_TOON
 	params_specular_mode = SpatialMaterial.SPECULAR_TOON
+	params_depth_draw_mode = SpatialMaterial.DEPTH_DRAW_ALWAYS
+	
 	
 	distance_fade_mode = SpatialMaterial.DISTANCE_FADE_PIXEL_ALPHA
 	distance_fade_min_distance = 0.1
